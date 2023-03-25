991e20dccfa2322cc83b69d3f9f54abd2cbfe562
Daniel <daniel.wolela@gmail.com>
2023-03-25 15:15:46 +0400

    new look and feel, added functionality to see nutrition value and cheaper items

--- app.py
+++ app.py
@@ -1,10 +1,56 @@
 from flask import Flask, jsonify, render_template, make_response
 import pandas as pd
 import random
+import openai
+import os
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+import re
 
 app = Flask(__name__)
-csv_file = 'menu_items.csv'
+csv_file = 'new_menu_items.csv'
 df = pd.read_csv(csv_file)
+openai.api_key = os.environ.get('OPENAI_API_KEY')
+#use openai to get nutrition info
+def get_nutrition_info(food_name):
+    # search for food name
+    print(f"searching for {food_name}")
+    nutrition_facts = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=f""""
+show nutritional fact and classify as healthy or indulgent 
+{food_name}
+provide a brief answer in json format. no explanation is need just the json formatted response. no disclaimer or any additional information is required
+
+Example"""+"""
+{
+  "name": "food name emoji of the food",
+  "nutrition": {
+    "ingredients": "value"
+  } | {"nutrition": "not found"},
+  "classification": "indulgent | healthy | not found"
+}
+add more ingredients: value pairs as needed
+
+don't replay with any other text that's not parsed!
+don't add Response: or any other text before the json response
+only reply with the json response allways in the same format both key and value must be in double quotes check if the response is valid json before submitting
+be creative with the emojis the more the better just don't replace the food name with emoji put it next to it and make sure it's a valid emoji
+use two or more emojis if you can't find a single emoji for the food name
+if the value is zero or empty string don't include it in the response
+""",
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.3,
+    ).choices
+    # if no results return empty dict
+    if len(nutrition_facts) == 0:
+        return {}
+    # if results return random result
+    else:
+        print(nutrition_facts[0].text)
+        return nutrition_facts[0].text
 
 def spin_the_wheel(df):
     random_index = random.randint(0, len(df) - 1)
@@ -15,6 +61,39 @@ def spin_the_wheel(df):
 def get_restaurants(df):
     restaurants = df['RestaurantName'].unique()
     return list(restaurants)
+
+# get similar food items in df based on the item returned from the wheel
+def get_similar_items(df, item):
+    # get restaurant names and price that has a similar item
+    similar_items = df.loc[df['FoodName'].str.contains(item)][['RestaurantName', 'Price']]
+    
+    # if no similar items return empty dict
+    if len(similar_items) == 0:
+        return {}
+    # if results return random result
+    else:
+        return similar_items.to_dict('records')
+
+
+
+def find_similar_food(df, food_name, restaurant_name, price, threshold=80):
+    def get_similarity(row):
+        return fuzz.token_set_ratio(row["FoodName"], food_name)
+
+    df["similarity"] = df.apply(get_similarity, axis=1)
+    similar_food_df = df[(df["similarity"] >= threshold) & (df["RestaurantName"] != restaurant_name) & (df["Price"].astype(float) < price)].drop("similarity", axis=1)
+    filter_columns = ['FoodName', 'RestaurantName', 'Price']
+    similar_food_df = similar_food_df[filter_columns].sort_values(by=['Price']).head(3)
+
+    # if no similar items return empty dict
+    if len(similar_food_df) == 0:
+        return {}
+    # if results return random result
+    else:
+        # serialize the dataframe to json
+        return similar_food_df.to_json(orient='records')
+
+
 
 @app.route('/')
 def index():
@@ -34,6 +113,33 @@ def spin():
     item = spin_the_wheel(df)
     print(item)
     return jsonify(item)
+    
+# search for nutrition info based on food name
+@app.route('/search/<food_restaurant_price>')
+def search(food_restaurant_price):
+    # search for food name
+    food_name, restaurant_name, price = food_restaurant_price.split(':')
+    # print(food_name, restaurant_name, price)
+    # price = re.search(r"[0-9]{:3}",price)
+    price_pattern = re.compile(r"([0-9]{1,3})")
+    price = price_pattern.search(price).group(1)
+    price = float(price)
+    
+    nutrition_facts = get_nutrition_info(food_name)
+    similar_food_df = find_similar_food(df, food_name, restaurant_name, price, threshold=70)
+    print(similar_food_df)
+    # if no results return empty dict
+    if len(nutrition_facts) == 0:
+        return jsonify({})
+    # if results return random result
+    elif len(similar_food_df) == 0:
+        return jsonify({"nutrition_facts":nutrition_facts, "similar_food": {}})
+    else:
+        print(nutrition_facts)
+        print("--------------------------")
+        print(similar_food_df)
+        return jsonify({"nutrition_facts": nutrition_facts, "similar_food": similar_food_df})
+    #
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5003)
+    app.run(debug=True,port=5005)
--- static/scripts.js
+++ static/scripts.js
@@ -9,10 +9,15 @@ const restaurantName = document.getElementById("restaurant-name");
 let spinsLeft = 100;
 
 function displayResult(item) {
-	foodName.textContent = "Food Name: " + item.FoodName;
-	description.textContent = "Description: " + item.Description;
-	price.textContent = "Price: " + item.Price;
-	restaurantName.textContent = "Restaurant Name: " + item.RestaurantName;
+	//add each item in span tag before displaying
+	document.querySelector(".food_name_span").textContent = item.FoodName;
+	document.querySelector(".description_span").textContent = item.Description;
+	document.querySelector(".price_span").textContent = item.Price + " AED";
+	document.querySelector(".restaurant_name_span").textContent =
+		item.RestaurantName;
+	document
+		.querySelector(".restaurant_name_span")
+		.classList.add("alert", "alert-info");
 }
 
 spinButton.addEventListener("click", () => {
@@ -44,30 +49,117 @@ spinButton.addEventListener("click", () => {
 		// Display the number of spins left
 	}
 });
-// Get the elements
-const dropdown = document.querySelector("#restaurant-picker .dropdown");
-const dropdownMenu = document.querySelector(
-	"#restaurant-picker .dropdown-menu"
-);
-const dropdownButton = document.querySelector(
-	"#restaurant-picker .dropdown-toggle"
-);
-const dropdownItems = dropdownMenu.querySelectorAll(".dropdown-item");
 
-// Add event listener to the button
-dropdownButton.addEventListener("click", function () {
-	dropdown.classList.toggle("active");
-	dropdownMenu.classList.toggle("active");
+foodName.addEventListener("click", (e) => {
+	e.AT_TARGET;
+	const foodName = e.target.innerText.trim();
+	// show loading spinner
+	const restaurantName = document.querySelector(
+		".restaurant_name_span"
+	).innerText;
+
+	const price = document.querySelector(".price_span").innerText;
+
+	const food_restaurant_price = foodName + ":" + restaurantName + ":" + price;
+
+	const loading_spinner = document.querySelector(".loading-spinner");
+	loading_spinner.style.display = "flex";
+	//disable the button by changing the color
+	// foodName.classList.add("disabled");
+	fetch(`/search/${food_restaurant_price}`)
+		.then((response) => response.json())
+		.then((item) => {
+			displayNutritionModal(item);
+			// remove loading spinner
+			loading_spinner.style.display = "none";
+		})
+		.catch((error) => {
+			console.log(error);
+		});
 });
 
-// Add event listeners to the dropdown items
-dropdownItems.forEach(function (item) {
-	item.addEventListener("click", function () {
-		dropdownItems.forEach(function (item) {
-			item.classList.remove("active");
+function displayNutritionModal(foodItem) {
+	const modal = document.querySelector(".modal");
+	const modalContent = modal.querySelector(".modal-content");
+	const modalFooterContent = modal.querySelector(".modal-footer-content");
+	const modalFooterTitle = modal.querySelector(".modal-footer-title h6");
+	console.log(foodItem["nutrition_facts"]);
+	nutritionFacts = JSON.parse(foodItem["nutrition_facts"]);
+	// console.log(foodItem["similar_food"]);
+	//check foodItem["similar_food"] is not empty using empty array
+	// add similar food to the footer as bootsrap cards
+	if (foodItem["similar_food"].length) {
+		similarFood = JSON.parse(foodItem["similar_food"]);
+		modalFooterContent.innerHTML = "";
+		console.log(similarFood);
+		modalFooterTitle.textContent = "Similar Foods At A Cheaper Price";
+		similarFood.forEach((food) => {
+			const card = document.createElement("div");
+			card.classList.add("card", "col-3", "m-2");
+			const cardBody = document.createElement("div");
+			cardBody.classList.add("card-body");
+			const cardTitle = document.createElement("h5");
+			cardTitle.classList.add("card-title");
+			cardTitle.textContent = food.FoodName + "🍔";
+			const cardText = document.createElement("p");
+			cardText.classList.add("card-text", "badge", "badge-dark");
+			cardText.textContent = food.RestaurantName + "👨🏽‍🍳";
+			// cardText.addClassList("badge", "badge-dark");
+			const cardPrice = document.createElement("p");
+			cardPrice.classList.add("card-text");
+			cardPrice.textContent = food.Price + " AED 🤑";
+			cardBody.appendChild(cardTitle);
+			cardBody.appendChild(cardText);
+			cardBody.appendChild(cardPrice);
+			card.appendChild(cardBody);
+			modalFooterContent.appendChild(card);
 		});
-		this.classList.add("active");
-		dropdownButton.innerHTML =
-			this.textContent + ' <span class="caret"></span>';
-	});
+	}
+
+	const heading = modalContent.querySelector("h2");
+	heading.textContent = nutritionFacts.name;
+
+	const classification = modalContent.querySelector(".classification");
+	classification.textContent = nutritionFacts.classification;
+	//if classification is "indulgetnt" then make the text red
+	if (nutritionFacts.classification === "indulgent") {
+		classification.classList.add("badge", "badge-danger", "text-white");
+	} else {
+		classification.classList.add("badge", "badge-success", "text-white");
+	}
+	//Update the classification tex to be Title Case
+	classification.textContent = classification.textContent.replace(
+		/indulgent|healthy/gi,
+		(match) => match[0].toUpperCase() + match.slice(1)
+	);
+
+	const nutritionData = nutritionFacts.hasOwnProperty("nutrition")
+		? nutritionFacts.nutrition
+		: nutritionFacts["Nutritional Facts"]?.[0];
+
+	const nutritionList = modalContent.querySelector(".nutrition-list");
+	nutritionList.innerHTML = ""; // Clear existing list items
+
+	if (nutritionData) {
+		for (const prop in nutritionData) {
+			const listItem = document.createElement("li");
+			listItem.textContent = `${prop}: ${nutritionData[prop]}`;
+			listItem.classList.add(
+				"list-group-item",
+				"d-flex",
+				"justify-content-between",
+				"align-items-center"
+			);
+			nutritionList.appendChild(listItem);
+		}
+	}
+
+	modal.style.display = "block";
+}
+
+// Add an event listener for the close button
+const modal = document.querySelector(".modal");
+const closeButton = modal.querySelector(".close");
+closeButton.addEventListener("click", () => {
+	modal.style.display = "none";
 });
